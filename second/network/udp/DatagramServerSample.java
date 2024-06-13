fcadcaca10d38ff6c54d770328e8c21665c019c6
jinkshower <askthem@naver.com>
2024-06-13 16:20:53 +0900

    udp - socket

---
+++ second/network/udp/DatagramServerSample.java
@@ -0,0 +1,47 @@
+package second.network.udp;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class DatagramServerSample {
+
+    public static void main(String[] args) {
+        DatagramServerSample sample = new DatagramServerSample();
+        sample.startServer();
+    }
+
+    public void startServer() {
+        DatagramSocket server = null;
+        try {
+            server = new DatagramSocket(9999);
+            int bufferLength = 256;
+            byte[] buffer = new byte[bufferLength];
+            DatagramPacket packet = new DatagramPacket(buffer, bufferLength);
+
+            while (true) {
+                System.out.println("Server : waiting for request");
+                server.receive(packet);
+                int dataLength = packet.getLength();
+                System.out.println("Server : received. Data length = " + dataLength);
+                String data = new String(packet.getData(), 0, dataLength);
+                System.out.println("Received Data : " + data);
+
+                if (data.equals("EXIT")) {
+                    System.out.println("Stop DatagramServer");
+                    break;
+                }
+            }
+            System.out.println("-------------------------");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(server != null) {
+                try {
+                    server.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
